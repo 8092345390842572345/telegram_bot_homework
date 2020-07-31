3948d51bd8d5347ee05eba8e4c72cd61176ea174
f1rA5h <57868199+f1rA5h@users.noreply.github.com>
2020-07-31 22:25:01 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,16 +1,17 @@
 import telebot
 from telebot import types
 import math
+from token import API_TOKEN as tk
 
 # список команд: send, register, start, help
 
-API_TOKEN = "1219283344:AAHNXmHuA8MltzpFJ5CLsPLaU5zMitTxN4o"    # токен бота
-
-bot = telebot.TeleBot(API_TOKEN)
+bot = telebot.TeleBot(tk)
 
 text1 = "send"                                                  # первый текст на кнопке клавиатуры
 text2 = "изменить данные учетной записи"                        # второой текст на кнопке клавиатуры
 text3 = "/register"
+
+dct = {}
 
 meetingText = "О, ты снова тут"                                 # приветственный текст
 registerText = 'введите свой класс и паралель через точку. \nПример: "8.6"'
@@ -21,10 +22,7 @@ helpText = "Короче, Меченый, я получил /help и в благ
 
 @bot.message_handler(commands=['1895001123581321345589'])
 def file_output(message):
-    doc = open("data.txt", "a+")
-    chat_id = message.chat.id
-    bot.send_document(chat_id, doc)
-    doc.close()
+    pass
 
 
 @bot.message_handler(commands=['start'])
@@ -32,13 +30,11 @@ def send_welcome(message):                                      # приветс
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(text1, text3) 
     msg = bot.reply_to(message, meetingText, reply_markup=markup)
-    bot.register_next_step_handler(msg, process_step)
 
 
 @bot.message_handler(commands=['help'])
 def help_function(message):                                      # функция help с описанием возможностей
     msg = bot.reply_to(message, helpText)
-    bot.register_next_step_handler(msg, process_step)
 
 
 @bot.message_handler(commands=['register'])
@@ -47,19 +43,14 @@ def register_function(message):
     bot.register_next_step_handler(msg, register_set)
     
 
-def register_set(message):      
-    doc = open("data.txt", "a+")                                # функция обработки паралели и класса при регистрации
+def register_set(message):                                      # функция обработки паралели и класса при регистрации
     chat_id = message.chat.id
-    msg = bot.reply_to(message, chat_id)
-    doc.write(str(chat_id))
-    doc.close()
     try:
         class_num = float(message.text)
+        try:
+            dct[chat_id]
     except BaseException:
         msg = bot.reply_to(message, errorText)
 
-
-def process_step(message):                                      # функция обработки кастомной клавиатуры бота
-    chat_id = message.chat.id
 
 bot.polling()
