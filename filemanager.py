5703ff48a9dc8cec656c7fa7777bdd2be64408ac
f1rA5h <57868199+f1rA5h@users.noreply.github.com>
2020-08-01 21:29:44 +0300

    Add files via upload

--- filemanager.py
+++ filemanager.py
@@ -1,13 +1,46 @@
 import sqlite3
 
-def intelisation():
-    conn = sqlite3.connect('dateBase.sqlite')
+conn = sqlite3.connect('dateBase.sqlite')
+cursor = conn.cursor()
 
-    cursor = conn.cursor()
+# it works. As a useless code
 
+reregRequest = """
+    UPDATE userData 
+    SET uClass = '?' 
+    WHERE uId = '?'
+"""
+
+regRequest = """
+    INSERT INTO userData VALUES (?,?)
+"""
+
+
+
+def intilisation():
     try:
         cursor.execute("""CREATE TABLE userData
-                    (id text, 
+                    (uId text, 
                     uClass text)""")
+    except BaseException:  # couse I don't know how to fix it
+        print("base hasn't created. It can already exist or this code sucks")
+
+
+# help me please
+
+def reg(uId, uClass):
+    try:
+        print("1")
+        cursor.execute(reregRequest, (uClass, uId))
+        conn.commit()
+        print("rereg")
     except BaseException:
-        pass
+        print("2")
+        cursor.execute(regRequest, (uClass, uId))
+        print("reg")
+    conn.commit()
+
+# fuck this code
+
+intilisation()
+reg("12", "2")
