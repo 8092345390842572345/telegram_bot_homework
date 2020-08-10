b62742cca80881025019841db3fd41dc8c2feabb
f1rA5h <57868199+f1rA5h@users.noreply.github.com>
2020-08-10 17:55:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ import threading
 import filemanager
 from PIL import Image
 import time
+import os
+from flask import Flask, request
+import logging
 
 lock = threading.Lock()
 filemanager.intilisation()
@@ -84,12 +87,30 @@ def hello(message):
     
 
 
-while True:
-    try:
-        bot.polling(none_stop=True)
+bot = telebot.TeleBot(token)
 
-    except Exception as e:
-        print(e)  # или просто print(e) если у вас логгера нет,
-        # или import traceback; traceback.print_exc() для печати полной инфы
-        time.sleep(15)
+# Здесь пишем наши хэндлеры
+
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://min-gallows.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
+
 filemanager.conn.close()
