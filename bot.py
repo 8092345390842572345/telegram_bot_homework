4ea5a2a1f49c052810d656d252cfbd2cae81e933
f1rA5h <57868199+f1rA5h@users.noreply.github.com>
2020-08-10 18:55:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from flask import Flask, request
 import logging
 try:
     mkdir("/photos")
-excect BaseExcepction:
+excepct BaseExcepction:
     pass
 lock = threading.Lock()
 filemanager.intilisation()
